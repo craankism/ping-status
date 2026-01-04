d21c871b601e4fc1796035fdc15504f63bb66c13
craankism <saschaschwellnus@gmail.com>
2026-01-04 17:31:48 +0100

    feat: create metrics server based on icmp requests

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	probing "github.com/prometheus-community/pro-bing"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func main() {
-	pinger, err := probing.NewPinger("www.google.com")
+func pinger(domains string) (ping float64, packetLoss float64) {
+	//icmp echo
+	pinger, err := probing.NewPinger(domains)
 	if err != nil {
 		panic(err)
 	}
@@ -17,12 +24,51 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	address := pinger.Statistics().Addr
-	ping := pinger.Statistics().AvgRtt // get send/receive/duplicate/rtt stats
-	packetLoss := pinger.Statistics().PacketLoss
-	TTL := pinger.Statistics().TTLs
-	fmt.Printf("%-15s %s\n", "Website:", address)
-	fmt.Printf("%-15s %d ms\n", "Ping:", ping.Milliseconds())
-	fmt.Printf("%-15s %.2f%%\n", "Packet Loss:", packetLoss)
-	fmt.Printf("%-15s %v\n", "TTLs received:", TTL)
+	ping = float64(pinger.Statistics().AvgRtt)
+	packetLoss = pinger.Statistics().PacketLoss
+	return ping, packetLoss
+}
+
+func main() {
+	//env var for domains
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	domains := os.Getenv("DOMAINS")
+
+	//prometheus
+	//set new gauges
+	ping := prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		Name:      "ping",
+		Subsystem: domains,
+		Namespace: "craankism",
+		Help:      "Checking ping of server in ms",
+	},
+		func() float64 {
+			ping, _ := pinger(domains)
+			return ping
+		},
+	)
+	packetLoss := prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		Name:      "packet_loss",
+		Subsystem: domains,
+		Namespace: "craankism",
+		Help:      "Checking packet loss of server in %",
+	},
+		func() float64 {
+			_, packetLoss := pinger(domains)
+			return packetLoss
+		},
+	)
+	//register gauges
+	prometheus.MustRegister(ping)
+	prometheus.MustRegister(packetLoss)
+
+	//remove go collector
+	prometheus.Unregister(collectors.NewGoCollector())
+	prometheus.Unregister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
 }
